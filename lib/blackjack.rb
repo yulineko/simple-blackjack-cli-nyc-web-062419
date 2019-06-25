b1fc894c850328a82a12d128598af2db937803b3
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-25 02:12:54 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,60 +1,51 @@
 def welcome
-  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
-  card = rand(1..11)
+  card = rand(1..11) #reuse the randomize method by setting it equal to a variable
 end
 
 def display_card_total(card_total)
-  # code #display_card_total here
   puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
-  input = gets.chomp
+  user_input = gets.chomp
 end
 
 def end_game(card_total)
-  # code #end_game here
-  puts "Sorry, you hit 27. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
   card_one = deal_card
   card_two = deal_card
   card_total = card_one + card_two
-  display_card_total(card_total)
+  display_card_total(card_total) #once you have figured out the sum of your two cards, you can pass the total in as an argumnt so your phrase is displayed.
   return card_total
 end
 
 def hit?(card_total)
-  # code hit? here
-  
-  prompt_user
-  input = get_user_input
-  if input == "h"
-    card_total += deal_card
-  elsif input == "s"
-    card_total
-  else 
+  prompt_user #ask the user if they want to hit or stay
+  user_input = get_user_input #set a variable to use for your if/else statement
+  if user_input == "h"
+    card_total += deal_card #increase your former total by the new number given by dealing another card
+  elsif user_input == "s"
+    card_total #return the number and prompt user to hit again
+  else
     invalid_command
   end
 end
 
 def invalid_command
-  # code invalid_command here
-  
   puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
@@ -62,12 +53,12 @@ end
 #####################################################
 
 def runner
-  welcome  
-  card_total = initial_round
-  until card_total == 21
+  welcome #introduce the game
+  sleep 0.75
+  card_total = initial_round #determine your current total & tell user what number they currently have
+  until card_total > 21 #continue game until user loses
     card_total = hit?(card_total)
-    card_total = display_card_total(card_total)
+    display_card_total(card_total)
   end
-  end_game(card_total)
+  end_game(card_total) #prints out once user has lost
 end
-    
