74a563f2dce3503dff18f43d944d7740d9307027
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-25 02:19:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,8 +63,8 @@ end
 
 def runner
   welcome 
-  card_total = initial_round #determine your current total & tell user what number they currently have
-  until card_total > 21 #continue game until user loses
+  card_total = initial_round 
+  until card_total > 21 
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
