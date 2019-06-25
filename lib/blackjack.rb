aea21d0573915e39c5067e2f62fd9f5a587d590c
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-06-25 02:14:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,7 @@ end
 
 def end_game(card_total)
   # code #end_game here
-  puts "Sorry, you hit 27. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
